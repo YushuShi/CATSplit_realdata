38b92e24767fd7e42288223b552dd4437afbbc2c
Yushu Shi <36617896+YushuShi@users.noreply.github.com>
2024-10-14 17:51:05 -0400

    correct the bug in using lasso for binary outcomes

--- real_analysis.R
+++ real_analysis.R
@@ -14,17 +14,17 @@ source("preprocess.R")
 source("Split_fun.R")
 source("DS_Mann_fun.R")
 
-## You can load the result directly if you don't have enough time to run
-if(file.exists("result.RData")) load("result.RData")
-
-## Mann-Whitney
-Man_selected <- Mann_WhitU(otu_table, taxonomy_table, meta_table, qval_bound = 0.05)
-Man_selected
+# ## You can load the result directly if you don't have enough time to run
+# if(file.exists("result.RData")) load("result.RData")
+# 
+# ## Mann-Whitney
+# Man_selected <- Mann_WhitU(otu_table, taxonomy_table, meta_table, qval_bound = 0.05)
+# Man_selected
 
 
 ## LASSO with data splitting
 meta_bin <- ifelse(meta_table$BinOutcomes == "R", 1, 0)
-DS_selected <- DS(otu_table, meta_bin, taxonomy_table, num_split = 50, qval_bound = 0.05)
+DS_selected <- DSBin(otu_table, meta_bin, taxonomy_table, num_split = 50, qval_bound = 0.05)
 DS_selected
 
 ## CATSplit
