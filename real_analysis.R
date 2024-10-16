438f6a03db755118e141c668d558561d41fbca0f
Yushu Shi <36617896+YushuShi@users.noreply.github.com>
2024-10-16 12:05:45 -0400

    convert otu table to abundance
    convert otu table to abundance for mann whitney

--- real_analysis.R
+++ real_analysis.R
@@ -13,12 +13,14 @@ source("helper_function.R")
 source("preprocess.R")
 source("Split_fun.R")
 source("DS_Mann_fun.R")
-
+source("otutabletoabundance.R")
+abundanceTable<-otutabletoabundance(otu_table)
+apply(abundanceTable,2,sum) # double check if abundance sum to 1 for each obs
 ## You can load the result directly if you don't have enough time to run
-if(file.exists("result.RData")) load("result.RData")
+#if(file.exists("result.RData")) load("result.RData")
 
 ## Mann-Whitney
-Man_selected <- Mann_WhitU(otu_table, taxonomy_table, meta_table, qval_bound = 0.05)
+Man_selected <- Mann_WhitU(abundanceTable, taxonomy_table, meta_table, qval_bound = 0.2)
 Man_selected
 
 
